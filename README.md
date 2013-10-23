530773980dd658b37f4d80bf597af025c91ce4f1
David Négrier <d.negrier@thecodingmachine.com>
2013-10-23 11:16:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+What is this package?
+=====================
+
+This package contains PHP objects representing common HTML form elements, along renderers.
+
+Why bother?
+-----------
+
+TODO
+
+Note: if you are looking for a low-level representation of HTML tags as PHP objects, head over to the [mouf/html.tags package](http://mouf-php.com/packages/mouf/html.widgets.form-elements).
+
+
+List of supported form elements
+-------------------------------
+
+TODO
+
+Mouf package
+------------
+
+This package is part of [Mouf](http://mouf-php.com), an effort to ensure good developing practices by providing a graphical dependency injection framework.
---
+++ src/Mouf/Html/Widgets/Form/FormElementInstaller.php
@@ -0,0 +1,24 @@
+<?php
+namespace Mouf\Html\Widgets\Form;
+
+use Mouf\Installer\PackageInstallerInterface;
+use Mouf\MoufManager;
+use Mouf\Html\Renderer\RendererUtils;
+
+/**
+ * A logger class that writes messages into the php error_log.
+ */
+class FormElementInstaller implements PackageInstallerInterface {
+
+    /**
+     * (non-PHPdoc)
+     * @see \Mouf\Installer\PackageInstallerInterface::install()
+     */
+    public static function install(MoufManager $moufManager) {
+               	// Let's create the renderer
+		RendererUtils::createPackageRenderer($moufManager, "mouf/html.widgets.form-element");
+
+        // Let's rewrite the MoufComponents.php file to save the component
+        $moufManager->rewriteMouf();
+    }
+}
